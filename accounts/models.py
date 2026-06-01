4f9a7ab62fa43cdd74100999c2112cffb5009721
kumar yedida <kumaryedida339@gmail.com>
2026-06-01 23:34:37 +0530

    user model login,logout

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+
+class User (models.Model):
+    ROLE_CHOICES = (
+        ('principal', 'Principal'),
+        ('dean', 'Dean'),
+        ('hod', 'HOD'),
+        ('committee_coordinator', 'Committee Coordinator'),
+        ('department_incharge', 'Department Incharge'),
+        ('faculty', 'Faculty'),
+    )
+
+    username = models.CharField(max_length=100)
+    register_no = models.CharField(max_length=10, unique=True,blank=True)
+    email = models.EmailField(unique=True,blank=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
+    points=models.IntegerField(default=0,blank=True,null=True)
+    
+    def save(self, *args, **kwargs):
+        # Hash the password before saving
+        if not self.password.startswith('pbkdf2_'):  # Only hash on creation
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.username
+    
+class BlacklistedToken(models.Model):
+        token = models.TextField(unique=True)
+        blacklisted_at = models.DateTimeField(auto_now_add=True)
+    
---
+++ accounts/permissions.py
@@ -0,0 +1,56 @@
+from rest_framework.permissions import BasePermission
+
+from accounts.models import BlacklistedToken
+from .token_jwt import decode_token, get_token_from_request
+import jwt
+
+
+def get_user_from_request(request):
+    token = get_token_from_request(request)
+    if token and BlacklistedToken.objects.filter(token=token).exists():
+        return None
+    elif token:
+        try:
+            user = decode_token(token)
+            return user
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+    return None
+
+
+class IsAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        user = get_user_from_request(request)
+        return user is not None
+    
+class IsPrincipal(BasePermission):
+    def has_permission(self, request, view):
+        user = get_user_from_request(request)
+        return user and user['role'] == 'principal'
+
+class IsDean(BasePermission):
+    def has_permission(self, request, view):
+        user = get_user_from_request(request)
+        return user and user['role'] == 'dean'
+    
+class IsHOD(BasePermission):
+    def has_permission(self, request, view):
+        user = get_user_from_request(request)
+        return user and user['role'] == 'hod'
+
+class IsCommittee_Coordinator(BasePermission):
+    def has_permission(self, request, view):
+        user = get_user_from_request(request)
+        return user and user['role'] == 'committee_coordinator'
+
+class IsDepartment_Incharge(BasePermission):
+    def has_permission(self, request, view):
+        user = get_user_from_request(request)
+        return user and user['role'] == 'department_incharge'
+
+class IsFaculty(BasePermission):
+    def has_permission(self, request, view):
+        user = get_user_from_request(request)
+        return user and user['role'] == 'faculty'
