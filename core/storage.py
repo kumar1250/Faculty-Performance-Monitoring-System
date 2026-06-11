b7e78faa0e789aa91e1624f47050a7e1521fd191
k-mounica <kanchustambhammounica@gmail.com>
2026-06-12 00:19:38 +0530

    chairing session module

--- core/storage.py
+++ core/storage.py
@@ -3,3 +3,6 @@ from storages.backends.s3 import S3Storage
 
 class CertificateStorage(S3Storage):
     location = "certificates"
+
+class Sessioncertificate(S3Storage):
+    location = "session_certificates"
