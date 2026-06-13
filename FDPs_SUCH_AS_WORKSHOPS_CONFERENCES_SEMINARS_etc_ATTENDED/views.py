f833a523ca143c994ea249df54969019d0737ade
kumar yedida <kumaryedida339@gmail.com>
2026-06-13 19:47:01 +0530

    fdps modulesj

--- FDPs_SUCH_AS_WORKSHOPS_CONFERENCES_SEMINARS_etc_ATTENDED/views.py
+++ FDPs_SUCH_AS_WORKSHOPS_CONFERENCES_SEMINARS_etc_ATTENDED/views.py
@@ -370,12 +370,11 @@ class FDPsAttendedViewSet(ViewSet):
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             region_name=settings.AWS_S3_REGION_NAME
         )
-        key = f"FDPs_Attended_Storage/{fdp.certificate_file.name}"
         url = s3.generate_presigned_url(
             "get_object",
             Params={
                 "Bucket": settings.AWS_STORAGE_BUCKET_NAME,
-                "Key": key
+                "Key": f"FDPs_Attended_Storage/{fdp.certificate_file.name}"
             },
             ExpiresIn=3600
         )
